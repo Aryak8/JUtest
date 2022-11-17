0f26570c894cf49a8d515cf1cc1967d1e073ee7e
aryakagrahari <aryakagrahari@localhost>
2022-11-17 15:07:22 +0530

    ju

---
+++ src/JUtestpackage/JUfunctions.java
@@ -0,0 +1,15 @@
+package JUtestpackage;
+
+public class JUfunctions {
+	public static int add(int a,int b)
+	{  
+		return(a+b);
+
+	}
+	
+	public static String addstring(String x  , String y)
+	{
+	return(x+y);
+	}
+
+}
